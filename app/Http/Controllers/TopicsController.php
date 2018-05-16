96c91a13bb965c11f73d132e055a23e9d6979ee0
zhangtaohua <zthvivid@163.com>
2018-05-16 23:11:33 +0800

    Author can delete topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -58,7 +58,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功!');
 	}
 
 	public function destroy(Topic $topic)
@@ -66,7 +66,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', '删除成功！');
+		return redirect()->route('topics.index')->with('message', '成功删除！');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
