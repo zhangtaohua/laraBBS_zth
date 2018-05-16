68de85066d55784ae742585c2e27d1d5120991d5
zhangtaohua <zthvivid@163.com>
2018-05-16 10:57:02 +0800

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,10 @@ Auth::routes();
 // $ rm app/Http/Controllers/HomeController.php
 // $ rm resources/views/home.blade.php
 //Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+// 上面代码等同于下面三个路由
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
